f084ee34c08566fe261438dd6e950a2ef43ecd95
Meklit Shiferaw <167539798+Meklitshiferaw@users.noreply.github.com>
2026-01-09 16:20:23 +0300

    updated shared infrastructure

--- src/Shared/Shared.Infrastructure/EventBus/IEventSubscriber.cs
+++
@@ -1,9 +0,0 @@
-using Shared.Domain.Events;
-
-namespace Shared.Infrastructure.EventBus;
-
-public interface IEventSubscriber<in TEvent> where TEvent : DomainEvent
-{
-    Task HandleAsync(TEvent domainEvent, CancellationToken cancellationToken = default);
-}
-
