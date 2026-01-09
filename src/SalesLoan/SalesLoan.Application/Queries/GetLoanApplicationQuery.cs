c9ede8eb9b71a8c4a8eb97d34bcac4f43f7b87eb
helinnatarekegn <150429939+helinnatarekegn@users.noreply.github.com>
2026-01-09 18:31:56 +0300

    Revert "Feature/sales loan"

--- src/SalesLoan/SalesLoan.Application/Queries/GetLoanApplicationQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using SalesLoan.Application.DTOs;
-
-namespace SalesLoan.Application.Queries;
-
-public record GetLoanApplicationQuery(Guid LoanApplicationId) : IRequest<LoanApplicationDto?>;
-
